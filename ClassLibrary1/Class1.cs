d3e440678f3514fa56af74a61fb80feddef100ec
wdq <1490747123@qq.com>
2017-09-19 11:57:26 +0800

    add files

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Double()
+        {
+            Console.WriteLine("1." + Assembly.GetEntryAssembly().FullName);
+            Console.WriteLine("2." + Assembly.GetExecutingAssembly().FullName);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Messaging;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp
+{ 
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Animal ani=new Cat();
+            (ani as Cat).Speak();
+        }
+        
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal.Speak");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Cat.Speak");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Dog.Speak");
+        }
+    }
+}
+
+
+ 
