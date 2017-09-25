d279e571546769d7e297cfcb4e844fb0b4f6e3df
wdq <1490747123@qq.com>
2017-09-25 17:55:47 +0800

    分析种子文件

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,51 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Messaging;
-using System.Net;
-using System.Net.Http;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace ConsoleApp
-{ 
-    class Program
-    { 
+{
+    /// <summary>
+    /// Summary description for Class1.
+    /// </summary>
+    public class Class1
+    {
         static void Main(string[] args)
         {
-            Animal ani=new Cat();
-            (ani as Cat).Speak();
+            string filename = Path.Combine(Directory.GetCurrentDirectory(), "lgcr.torrent");
+            using (var fs = new FileStream(filename, FileMode.Open))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    //le,de,n:str,ie 
+                    var dic = DecodeData(br);
+                }
+            }
         }
-        
-    }
-    class Animal
-    {
-        public virtual void Speak()
+
+        private static ItemBase DecodeData(BinaryReader br)
         {
-            Console.WriteLine("Animal.Speak");
+            var flag = br.PeekChar();
+            ItemBase item = null;
+            List<byte> ls = new List<byte>();
+            byte b = 0;
+            switch (flag)
+            {
+                case 'e':
+                    br.ReadByte();
+                    break;
+                case 'l'://列表
+                    br.ReadByte();
+                    var itemLs = new ListItem();
+                    var i = DecodeData(br);
+                    while (i != null && br.BaseStream.Position == br.BaseStream.Length - 2)
+                    {
+                        var val = DecodeData(br);
+                        itemLs.ListData.Add(i);
+                        i = DecodeData(br);
+                    }
+
+                    item = itemLs;
+                    break;
+                case 'd'://字典
+                    br.ReadByte();
+                    var itemDic = new DictionaryItem();
+                    var key = DecodeData(br);
+                    while (key != null && br.BaseStream.Position != br.BaseStream.Length)
+                    {
+                        var val = DecodeData(br);
+                        itemDic.DictionaryData[(key as StringItem).StringData] = val;
+                        key = DecodeData(br);
+                    }
+
+                    item = itemDic;
+                    break;
+                case 'i'://数字
+                    br.ReadByte();
+                    b = br.ReadByte();
+                    while (b != 'e')
+                    {
+                        ls.Add(b);
+                        b = br.ReadByte();
+                    }
+                    item = new NumberItem(int.Parse(Encoding.UTF8.GetString(ls.ToArray()))) { RawBytes = ls.ToArray() };
+                    break;
+                default://字符串 
+                    b = br.ReadByte();
+                    while (b != ':')
+                    {
+                        ls.Add(b);
+                        b = br.ReadByte();
+                    }
+                    var len = int.Parse(Encoding.UTF8.GetString(ls.ToArray()));
+                    var bufStr = br.ReadBytes(len);
+                    var data = Encoding.UTF8.GetString(bufStr);
+                    item = new StringItem(data) { RawBytes = bufStr };
+                    break;
+            }
+            return item;
         }
     }
 
-    class Cat : Animal
+    public class ItemBase
     {
-        public void Speak()
-        {
-            Console.WriteLine("Cat.Speak");
-        }
+        public ItemType ItemType { get; set; }
+        public byte[] RawBytes { get; set; }
     }
 
-    class Dog : Animal
+    public class StringItem : ItemBase
     {
-        public override void Speak()
+        public StringItem(string data)
         {
-            Console.WriteLine("Dog.Speak");
+            StringData = data;
+            ItemType = ItemType.String;
         }
+        public string StringData { get; set; }
+    }
+    public class NumberItem : ItemBase
+    {
+        public NumberItem(int num)
+        {
+            NumberData = num;
+            ItemType = ItemType.Number;
+        }
+        public int NumberData { get; set; }
+    }
+
+    public class ListItem : ItemBase
+    {
+        public ListItem()
+        {
+            ItemType = ItemType.List;
+        }
+        public List<ItemBase> ListData { get; set; } = new List<ItemBase>();
+    }
+
+    public class DictionaryItem : ItemBase
+    {
+        public DictionaryItem()
+        {
+            ItemType = ItemType.Dictionary;
+        }
+        public Dictionary<string, ItemBase> DictionaryData { get; set; } = new Dictionary<string, ItemBase>();
+    }
+
+    public enum ItemType
+    {
+        String, Number, List, Dictionary
     }
 }
-
-
- 
